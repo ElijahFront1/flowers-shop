193cdc86b8bc27765c0fd9d72aca00be7f3be7ee
Span415 <64959991+Span415@users.noreply.github.com>
2021-03-08 19:01:54 +0300

    Update Navbar.jsx

--- src/components/header/Navbar/Navbar.jsx
+++ src/components/header/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
             <nav>
                 <div className="top-navigation">
                     <ul>
-                        <li className="top-navigation__item"><NavLink to="/home" activeClassName="activeLink">ГЛАВНАЯ</NavLink></li>
+                        <li className="top-navigation__item"><NavLink to="/" activeClassName="activeLink">ГЛАВНАЯ</NavLink></li>
                         <li className="top-navigation__item"><NavLink to="/shop" activeClassName="activeLink">МАГАЗИН</NavLink></li>
                         <li className="top-navigation__item"><NavLink to="/gallery" activeClassName="activeLink">ГАЛЕРЕЯ</NavLink></li>
                         <li className="top-navigation__item"><NavLink to="/blog" activeClassName="activeLink">БЛОГ</NavLink></li>
